3cacbde380eacccb7ce2909a35407b581f94e757
deLiero <alkh34@yandex.ru>
2016-12-14 20:51:29 +0300

    add basic server

--- src/server/app.js
+++ src/server/app.js
@@ -1,1 +1,13 @@
 const http = require("http");
+const util = require("util");
+
+// PARAMS
+const PORT = 8080;
+
+http.createServer((req, res) => {
+    res.end(req.url);
+    //console.log(util.inspect(req));
+    console.log("Got %s request. Url:%s", req.method, req.url);
+}).listen(PORT);
+
+console.log("SERVER: OK!\nPORT: %d\n", PORT);
